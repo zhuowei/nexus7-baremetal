d82bbe9821bc5ce923f0d3cd5757d951381a5126
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2013-03-19 16:07:39 -0700

    Print out current PC as binary

--- fail1/bootloader05.c
+++ fail1/bootloader05.c
@@ -10,6 +10,7 @@
 
 #define ARMBASE 0x8000
 #define VIDEOBASE 0xabe01000
+#define WIDTH 800
 
 extern void PUT32 ( unsigned int, unsigned int );
 extern void PUT16 ( unsigned int, unsigned int );
@@ -34,9 +35,43 @@ int notmain ( void )
 
     unsigned int crc;
 
-    for (int i = 0; i < 0x100000; i+=4) {
-        PUT32(VIDEOBASE + i, i);
+    unsigned int currentPc = GETPC();
+
+    for (int y = 0; y < 16; y++) {
+        for (int x = 0; x < (32 << 4); x++) {
+            int color = 0x808080;
+            if (x >> 4 & 0x1) color = 0xffffff;
+            PUT32(VIDEOBASE + (x * 4) + (y * 4 * WIDTH), color);
+        }
     }
+
+    for (int y = 16; y < 32; y++) {
+        for (int x = 0; x < (32 << 4); x++) {
+            int color = 0;
+            if (currentPc & (1 << ((x >> 4) % 32))) color = 0xffffff;
+            PUT32(VIDEOBASE + (x * 4) + (y * 4 * WIDTH), color);
+        }
+    }
+
+    /*for (int y = 0; y < 16; y++) {
+    for (int i = 0; i < WIDTH; i++) {
+        int color = 0;
+	if (y < 4) {
+		color = 0xffffff;
+	}
+        else if (currentPc & (1 << ((i >> 2) % 32))) {
+		color = 0xffffff;
+	} else {
+		if (i >> 2 & 1) color = 0xff0000;
+	}
+        PUT32(VIDEOBASE + (i * 4) + (y * 4 * WIDTH), color);
+    }*/
+    if (currentPc & 0xffff0000 == 0x80a00000) {
+    for (int i = 0; i < WIDTH; i++) {
+        PUT32(VIDEOBASE + (i * 4), 0xffffff);
+    }
+    }
+
     while(1) {}
     return(0);
 }
