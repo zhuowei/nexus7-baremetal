e34b4b083019ab2e13f1a2d328d3d0512cfb15fa
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2019-01-06 22:03:00 -0800

    pixel3hello: result of binary searching for the framebuffer

--- pixel3hello/bootloader05.c
+++ pixel3hello/bootloader05.c
@@ -1,6 +1,6 @@
 #define MPM2_MPM_PS_HOLD                     0x0C264000
 
-static void clearram() {
+static void* findframebuffer() {
 	// trying to find the framebuffer by brute force.
 	// zeroes all the ram.
 	// all memory regions from /proc/iomem
@@ -9,21 +9,25 @@ static void clearram() {
 	// this gets the screen to blank out before rebooting.
 	// implying that scanout is still turned on after booting kernel
 	// unlike Nexus 6P.
-/* process of elimination: it's in the last region
-for (int* p = (int*)0x81000000ull; p <= (int*)0x856fffffull; p++){ *p=0xff00ff00; }
-for (int* p = (int*)0x85d00000ull; p <= (int*)0x85dfffffull; p++){ *p=0xff00ff00; }
-for (int* p = (int*)0x85f00000ull; p <= (int*)0x85fbffffull; p++){ *p=0xff00ff00; }
-for (int* p = (int*)0x88f00000ull; p <= (int*)0x8aafffffull; p++){ *p=0xff00ff00; }
-for (int* p = (int*)0x8c417000ull; p <= (int*)0x8c4fffffull; p++){ *p=0xff00ff00; }
-*/
-for (int* p = (int*)0x99c00000ull; p <= (int*)0xa17fffffull; p++){ *p=0xff00ff00; }
-//for (int* p = (int*)0xa1c11000ull; p <= (int*)0x17e09ffffull; p++){ *p=0; }
+	for (unsigned int* p = (unsigned int*)0x9cb80000ull; p <= (unsigned int*)0x9d9fffffull; p++) {
+		*p = 0xff00ff00u;
+	}
+	return 0;
+}
+static void not_memset(void* a, int v, unsigned long long size) {
+	char* p = a;
+	for (unsigned long long i = 0; i < size; i++) {
+		p[i] = v;
+	}
 }
 int notmain (void *dtb) {
-	clearram();
+	void* fb = findframebuffer();
+	for (int i = 0; i < 0x40000000; i++) {
+		asm volatile("");
+	}
 	// simplest test code: just reboots the processo
-	unsigned int* hold = (unsigned int*)MPM2_MPM_PS_HOLD;
-	*hold = 0;
+	//unsigned int* hold = (unsigned int*)MPM2_MPM_PS_HOLD;
+	//*hold = 0;
 	while(1) {
 	}
 	return 0;
