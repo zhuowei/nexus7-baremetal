8b0da4e622e89023db042eaeb97b3839d50d4a73
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2019-01-06 21:31:00 -0800

    pixel3hello: filled regions with green one by one, it's the last region.

--- pixel3hello/bootloader05.c
+++ pixel3hello/bootloader05.c
@@ -9,12 +9,14 @@ static void clearram() {
 	// this gets the screen to blank out before rebooting.
 	// implying that scanout is still turned on after booting kernel
 	// unlike Nexus 6P.
-for (int* p = (int*)0x81000000ull; p <= (int*)0x856fffffull; p++){ *p=0; }
-for (int* p = (int*)0x85d00000ull; p <= (int*)0x85dfffffull; p++){ *p=0; }
-for (int* p = (int*)0x85f00000ull; p <= (int*)0x85fbffffull; p++){ *p=0; }
-for (int* p = (int*)0x88f00000ull; p <= (int*)0x8aafffffull; p++){ *p=0; }
-for (int* p = (int*)0x8c417000ull; p <= (int*)0x8c4fffffull; p++){ *p=0; }
-for (int* p = (int*)0x99c00000ull; p <= (int*)0xa17fffffull; p++){ *p=0; }
+/* process of elimination: it's in the last region
+for (int* p = (int*)0x81000000ull; p <= (int*)0x856fffffull; p++){ *p=0xff00ff00; }
+for (int* p = (int*)0x85d00000ull; p <= (int*)0x85dfffffull; p++){ *p=0xff00ff00; }
+for (int* p = (int*)0x85f00000ull; p <= (int*)0x85fbffffull; p++){ *p=0xff00ff00; }
+for (int* p = (int*)0x88f00000ull; p <= (int*)0x8aafffffull; p++){ *p=0xff00ff00; }
+for (int* p = (int*)0x8c417000ull; p <= (int*)0x8c4fffffull; p++){ *p=0xff00ff00; }
+*/
+for (int* p = (int*)0x99c00000ull; p <= (int*)0xa17fffffull; p++){ *p=0xff00ff00; }
 //for (int* p = (int*)0xa1c11000ull; p <= (int*)0x17e09ffffull; p++){ *p=0; }
 }
 int notmain (void *dtb) {
