cfe39eade46781e15cf7df9bdf5f30805999b47c
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2019-01-06 22:14:40 -0800

    pixel3hello: green most of the screen

--- pixel3hello/bootloader05.c
+++ pixel3hello/bootloader05.c
@@ -9,7 +9,7 @@ static void* findframebuffer() {
 	// this gets the screen to blank out before rebooting.
 	// implying that scanout is still turned on after booting kernel
 	// unlike Nexus 6P.
-	for (unsigned int* p = (unsigned int*)0x9d900000ull; p <= (unsigned int*)0x9db00000ull; p++) {
+	for (unsigned int* p = (unsigned int*)0x9d500000ull; p <= (unsigned int*)0x9e400000ull; p++) {
 		*p = 0xff00ff00u;
 	}
 	return 0;
